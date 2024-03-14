c829c88a281dd5b754310d68c67f66d6d01f44ef
baris <baris@BARIS_SOMEROGLU>
2024-03-14 17:40:09 +0300

    Add project files.

---
+++ MathFunctions/MathFunctions.cpp
@@ -0,0 +1,24 @@
+﻿/*
+In this code we will use some cmath's functions.
+
+Developer: Barış Someroğlu
+Date: 14.03.2024 - 5:40 p.m.
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	cout << "sin45: " << sin(45) << endl;
+	cout << "cos90: " << cos(90) << endl;
+	cout << "Karekok 36: " << sqrt(36) << endl;
+	cout << "Mutlak Deger -6: " << abs(-6) << endl;
+	cout << "6 uzeri 3: " << pow(6, 3) << endl;
+
+	cin.get(); // this will wait 
+
+	return 0;
+}
